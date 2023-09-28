0d61b94fb3b183f382304600117a39d14b3568a3
Jiayang Sun <sjyhehe@gmail.com>
2023-09-27 22:55:08 -0500

    Use openai lib instead

--- task/transcript_task.py
+++ task/transcript_task.py
@@ -1,9 +1,7 @@
 import logging
-import aiohttp
-import json
 import asyncio
-import os
-import base64
+import openai
+
 
 from task.task import Task, Request, Response
 from lib.video import Video
@@ -17,21 +15,19 @@ OPENAI_DOMAIN = "https://api.openai.com/v1/"
 
 class TranscriptRequest(Request):
     videos: List[Video]
+    language: str
 
 
 class TranscriptResponse(Response):
+    # TODO only return videos with transcript
     videos: List[Video]
 
 
 class TranscriptTask(Task):
-    def init(self) -> None:
-        self.headers = {
-            "Authorization": f"Bearer {OPENAI_API_KEY}",
-            "Content-Type": "application/json",
-        }
 
-    # TODO check out how to async this with openai lib
     async def transcribe(self, video: Video) -> Video:
+        openai.api_key = OPENAI_API_KEY
+
         prompts = []
         snippet = video.snippet
 
@@ -42,51 +38,22 @@ class TranscriptTask(Task):
         if "description" in snippet.keys():
             prompts.append(snippet["description"])
 
-        """
-        - file: The audio file to transcribe, in one of these formats: mp3, mp4, mpeg, mpga, m4a, wav, or webm.
-        example: '@/path/to/file/audio.mp3'
-        - model: ID of the model to use. Only whisper-1 is currently available. E.g 'whisper-1'
-        - prompt: An optional text to guide the model's style or continue a previous audio segment.
-        The prompt should match the audio language.
-        - response_format: The format of the transcript output, in one of these options: json, text,
-        srt, verbose_json, or vtt.
-        - temperature: The sampling temperature, between 0 and 1. Higher values like 0.8 will make the
-        output more random, while lower values like 0.2 will make it more focused and deterministic. If
-        set to 0, the model will use log probability to automatically increase the temperature until certain
-        thresholds are hit.
-        - language: The language of the input audio. Supplying the input language in ISO-639-1 format will
-        improve accuracy and latency.
-        """
-       
         with open(video.path, "rb") as file:
-            file_content = file.read()
-        request = {
-            'file': base64.b64encode(file_content).decode('utf-8'),
-            'model': 'whisper-1',
-            'prompt': ';'.join(prompts),
-            'response_format': 'srt',
-        }
-        # TODO debug return code 400 and 502 when internet came back
-        async with aiohttp.ClientSession(headers=self.headers) as session:
-            # TODO handle aiohttp.client_exceptions.ClientConnectorError
-            async with session.post(
-                f"{OPENAI_DOMAIN}/audio/transcriptions",
-                data=request,
-            ) as rsp:
-                print(rsp.status)
-                if rsp.status == 200:
-                    video.transcript = await rsp.json()
-                    logger.debug(f"Got response:\n {str( video.transcript)[:600]}")
-                else:
-                    # TODO handle non-200 return code.
-                    print(f"Got error {rsp.status}, {rsp}")
+            transcript = await openai.Audio.atranscribe(
+                model="whisper-1",
+                file=file,
+                prompt=";".join(prompts),
+                response_format="srt",
+                language="zh",
+            )
+            video.transcript = {"srt": transcript}
         return video
 
     async def start(self, req: TranscriptRequest) -> TranscriptResponse:
 
         processed_videos = []
         for video in req.videos:
-            processed = await self.transcribe(video)
+            processed = await self.transcribe2(video)
             processed_videos.append(processed)
 
         return TranscriptResponse(videos=processed_videos)
@@ -104,7 +71,7 @@ async def test_transcript_a_video() -> None:
         snippet={"channelTitle": "智胜", "description": "女同学"},
         transcript={}
     )
-    rsp = await task.start(TranscriptRequest(videos=[video]))
+    rsp = await task.start(TranscriptRequest(videos=[video], language="zh"))
     assert len(rsp.videos) == 1, "The transcript task expect to get 1 response"
     print(rsp.videos[0].transcript)
 
